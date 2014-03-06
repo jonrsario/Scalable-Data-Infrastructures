d53e64264d95b624080c82e29c0d93838752cd13
Jonathan Rosario <archidynamics@yahoo.com>
2014-03-06 14:52:36 -0500

    Lab 2

---
+++ Day2Lecture.js
@@ -0,0 +1,47 @@
+//alert("JavaScript works!");
+
+/* Jonathan Rosario
+ * SDI 1403
+ * Lecture 2
+ */ 
+ 
+ // My Variables
+ var myHomeAddress = "123 Elm Street";
+ var myWorkAddress = "Full Sail University";
+ var milesToSchool = 6.5;
+ var doITeachToday = true
+ 
+ // My outputs
+ console.log("I live at " + myHomeAddress + ".");
+ console.log("I work at " + myHomeAddress + ".");
+ console.log("It is " + (milesToSchool + 2) + " miles to th campus.");
+ console.log("It is " + doITeachToday + " I teach on Thursdays.");
+
+/* Jonathan Rosario
+ * Lecture 2
+ * In-class Activity
+ */
+
+//My Variables
+var MyName = "Jonathan Rosario";
+var MyNickname = "\"Jon\"";
+var MyAge = 31;
+var MyCarStatus = true
+
+// My Outputs
+console.log("My name is " + MyName + ".");
+console.log("My nickname is " + MyNickname + ".");
+console.log("I am " + MyAge + "  years old.");
+console.log("It is " + MyCarStatus + " I have a car");
+
+MyCarStatus = confirm("Is my car running?");
+
+console.log("It is " + MyCarStatus + " that my car is running.")
+
+MyNickname = prompt("Enter your new age: ", "100");
+
+MyNickname = parseFloat(MyNickname);
+
+console.log("My new age plus 5 is " + (myNickname + 5) + ".");
+
+
